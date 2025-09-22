746b5120669222400e2491358e7b5e8b5368a79e
KartikAg13 <23803013@mail.jiit.ac.in>
2025-09-22 21:04:26 +0530

    start work

--- main.py
+++ main.py
@@ -1,8 +1,68 @@
-from graphic.game import run_random_episode
-from neural.model import NeuralNetwork
+from typing import override
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
 
-def main():
-	run_random_episode()
+import gymnasium as gym
 
-if __name__ == '__main__':
-    main()
+class GridEnv(gym.Env):
+    def __init__(self, size: int) -> None:
+        super(GridEnv, self).__init__()
+        # size of the grid
+        self.size = size
+
+        # spawn locations
+        self.agent_location = np.array([-1, -1], dtype=np.int32)
+        self.target_location = np.array([-1, -1], dtype=np.int32)
+
+        # observation space
+        self.surrounding_space = gym.spaces.Box(-1, 1, (4, 0), np.int32)
+        self.distance_space = gym.spaces.Box(0, 2*size, (2, 0), np.int32)
+        self.observation_space = gym.spaces.Dict({
+            "surroundings": self.surrounding_space,
+            "distances": self.distance_space
+        })
+
+        # action space
+        self.action_space = gym.spaces.Discrete(4)
+        self.direction = {
+            0: np.array([-1, 0]),   # left
+            1: np.array([0, 1]),    # up
+            2: np.array([1, 0]),    # right
+            3: np.array([0, -1])    # down
+        }
+
+    def _get_obs(self):
+        # returns the current state
+        return {
+            "agent": self.agent_location,
+            "target": self.target_location
+        }
+
+    def _get_info(self):
+        # returns the distance between snake and fruit
+        return {
+            "distance": np.linalg.norm(self.agent_location - self.target_location, ord=1)
+        }
+
+    @override
+    def reset(self, *, seed: int | None = None):
+        # make sure the env is reproducible
+        super().reset(seed=seed)
+
+        # spawn the agent
+        self.agent_location = self.np_random.integers(0, self.size, size=2, dtype=np.int32)
+
+        # spawn the target
+        self.target_location = self.agent_location
+        while np.array_equal(self.agent_location, self.target_location):
+            self.target_location = self.np_random.integers(0, self.size, size=2, dtype=np.int32)
+
+        observation = self._get_obs()
+        info = self._get_info()
+        return observation, info
+
+    def step(self, action: int):
+
+        direction = self.direction[action]
+        self.agent_location = np.clip(a=self.agent_location + direction, a_min=0, a_max=self.size - 1)
